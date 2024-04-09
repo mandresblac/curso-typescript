028b6e14398885e4f23bff272ff8d8702fead639
manuel blanco <mandresblac@gmail.com>
2024-04-09 16:54:28 -0500

    commit 4

---
+++ aprendiendoTypeScript/tiposDatos.js
@@ -0,0 +1,11 @@
+"use strict";
+let cuenta = "cuenta";
+let cuentaNumerica = 3000000;
+let cuentaActiva = false;
+// console.log(cuenta);
+// console.log(cuentaNumerica);
+// console.log(cuentaActiva);
+// Arrays
+const cuentas = ["cuentas1", "cuentas2", "cuentas3"];
+cuentas[3] = "cuenta4";
+console.log(cuentas);
---
+++ aprendiendoTypeScript/tiposDatos.ts
@@ -0,0 +1,12 @@
+let cuenta: string = "cuenta";
+let cuentaNumerica: number = 3_000_000;
+let cuentaActiva: boolean = false;
+// console.log(cuenta);
+// console.log(cuentaNumerica);
+// console.log(cuentaActiva);
+
+// Arrays
+const cuentas: string[] = ["cuentas1", "cuentas2", "cuentas3"];
+cuentas[3] = "cuenta4";
+
+console.log(cuentas);
