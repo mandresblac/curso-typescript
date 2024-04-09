007f3943b0002d11f4d74a730fc7e0afba4148b0
manuel blanco <mandresblac@gmail.com>
2024-04-09 17:25:13 -0500

    commit 5

--- aprendiendoTypeScript/tiposDatos.js
+++ aprendiendoTypeScript/tiposDatos.js
@@ -6,6 +6,22 @@ let cuentaActiva = false;
 // console.log(cuentaNumerica);
 // console.log(cuentaActiva);
 // Arrays
+// Opción 1 de declarar un array: string[]
 const cuentas = ["cuentas1", "cuentas2", "cuentas3"];
 cuentas[3] = "cuenta4";
 console.log(cuentas);
+// Opción 2 de declarar un array: Array<string>
+const animales = ["Perro", "Gato", "Gallina", "Loro"];
+// Tuplas
+let miTupla;
+miTupla = [true, "string", 123];
+//Enums
+var Roles;
+(function (Roles) {
+    Roles[Roles["Empleado"] = 0] = "Empleado";
+    Roles[Roles["Administrador"] = 1] = "Administrador";
+    Roles[Roles["Asistente"] = 2] = "Asistente";
+})(Roles || (Roles = {}));
+;
+let resultado = Roles.Administrador;
+console.log(resultado);
--- aprendiendoTypeScript/tiposDatos.ts
+++ aprendiendoTypeScript/tiposDatos.ts
@@ -6,7 +6,25 @@ let cuentaActiva: boolean = false;
 // console.log(cuentaActiva);
 
 // Arrays
+// Opción 1 de declarar un array: string[]
 const cuentas: string[] = ["cuentas1", "cuentas2", "cuentas3"];
 cuentas[3] = "cuenta4";
 
 console.log(cuentas);
+
+// Opción 2 de declarar un array: Array<string>
+const animales: Array<string> = ["Perro", "Gato", "Gallina", "Loro"];
+
+// Tuplas
+let miTupla: [boolean, string, number];
+miTupla = [true, "string", 123];
+
+//Enums
+enum Roles {
+  Empleado,
+  Administrador,
+  Asistente
+};
+
+let resultado =  Roles.Administrador;
+console.log(resultado);
