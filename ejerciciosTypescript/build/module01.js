3a16cd67365ee916873b3d613051dad127204596
manuel blanco <mandresblac@gmail.com>
2024-04-09 22:26:23 -0500

    commit 8

--- ejerciciosTypescript/build/module01.js
+++ ejerciciosTypescript/build/module01.js
@@ -3,3 +3,15 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(3, 6));
+let x; //* Explicitly declares x as a number type
+let y = 1; //* Implicitly declares y as a number type
+let z; //* Declares z without initializing it
+var ContractStatus;
+(function (ContractStatus) {
+    ContractStatus[ContractStatus["Permanent"] = 1] = "Permanent";
+    ContractStatus[ContractStatus["Temp"] = 2] = "Temp";
+    ContractStatus[ContractStatus["Apprentice"] = 3] = "Apprentice";
+})(ContractStatus || (ContractStatus = {}));
+let employeeStatus = ContractStatus.Temp;
+console.log(employeeStatus);
+console.log(ContractStatus[employeeStatus]);
--- ejerciciosTypescript/module01.ts
+++ ejerciciosTypescript/module01.ts
@@ -2,3 +2,17 @@ function addNumbers(x: number, y: number) {
   return x + y;
 }
 console.log(addNumbers(3, 6));
+
+let x: number;   //* Explicitly declares x as a number type
+let y = 1;       //* Implicitly declares y as a number type
+let z;           //* Declares z without initializing it
+
+enum ContractStatus {
+    Permanent = 1,
+    Temp,
+    Apprentice
+}
+
+let employeeStatus: ContractStatus = ContractStatus.Temp;
+console.log(employeeStatus);
+console.log(ContractStatus[employeeStatus]);
