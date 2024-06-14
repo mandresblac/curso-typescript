77c2959f61764c06b4e5a67cea11550afe539604
manuel blanco <mandresblac@gmail.com>
2024-06-14 18:29:41 -0500

    commit 20

--- typescript-para-principiantes/type.js
+++ typescript-para-principiantes/type.js
@@ -1,6 +1,6 @@
 "use strict";
 function area(shape, width, height) {
-    let area = width * height;
-    return `Yo soy ${shape} con un area de ${area} cm al cuadrado`;
+    const area = width * height;
+    return `Yo soy un ${shape} con un area de ${area} cm al cuadrado.`;
 }
 document.body.innerHTML = area("rectangulo", 30, 15);
