b9f82905f1c33a669c1ca7316354cf5d087a0f75
manuel blanco <mandresblac@gmail.com>
2024-06-14 19:20:10 -0500

    commit 21

--- typescript-para-principiantes/interface.js
+++ typescript-para-principiantes/interface.js
@@ -1,7 +1,14 @@
 "use strict";
-function area(shape) {
-    let area = shape.width * shape.height;
+;
+function area1(shape) {
+    const area = shape.width * shape.height;
     return `Yo soy un ${shape.name} con un area de ${area} cm cuadrados`;
 }
-console.log(area({ name: "rectangulo", width: 30, height: 15 }));
-console.log(area({ name: "cuadrado", width: 30, height: 30, color: "blue" }));
+console.log(area1({ name: "rectangulo", width: 30, height: 15 }));
+console.log(area1({ name: "cuadrado", width: 30, height: 30, color: "blue" }));
+console.log(area1({ name: "cuadrado", width: 20, height: 20 }));
+function suma(n1, n2) {
+    return n1 + n2;
+}
+;
+console.log(suma(10, 11));
--- typescript-para-principiantes/interface.ts
+++ typescript-para-principiantes/interface.ts
@@ -3,12 +3,18 @@ interface Shape {
   width: number;
   height: number;
   color?: string;
-}
+};
 
-function area(shape: Shape) {
-  let area = shape.width * shape.height;
+function area1(shape: Shape) {
+  const area = shape.width * shape.height;
   return `Yo soy un ${shape.name} con un area de ${area} cm cuadrados`;
 }
 
-console.log(area({name: "rectangulo", width: 30, height: 15})); 
-console.log( area( {name: "cuadrado", width: 30, height: 30, color: "blue"} ) );
+console.log(area1({name: "rectangulo", width: 30, height: 15})); 
+console.log( area1( {name: "cuadrado", width: 30, height: 30, color: "blue"} ) );
+console.log(area1({name: "cuadrado", width: 20, height: 20}));  
+
+function suma(n1: number, n2: number): number {
+  return n1 + n2;
+};
+console.log(suma(10, 11));
